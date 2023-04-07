c9fe55a4e882652c43a8217c07eef224418e4a94
dhwpdnr <ink1237@gmail.com>
2023-04-07 16:42:30 +0900

    [feat] react hook clean up

--- components/src/App.js
+++ components/src/App.js
@@ -1,87 +1,109 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="container">
       <h1>Hello world</h1>
-      <FuncComp initNumber={2}/>
-      <ClassComp initNumber={2}/>
+      <FuncComp initNumber={2} />
+      <ClassComp initNumber={2} />
     </div>
   );
 }
 var funcStyle = "color:blue";
 var funcId = 0;
-function FuncComp(props){
+function FuncComp(props) {
   const [number, setNumber] = useState(props.initNumber);
-  const [_date, setDate] = useState((new Date()).toString());
+  const [_date, setDate] = useState(new Date().toString());
 
   // side effect
-  useEffect(function(){
-    console.log("%cfunc => useEffect (componentDidMount & componentDidUpdate) A" + (++funcId), funcStyle);
-    document.title = number + ' : ' + _date;
+  useEffect(function () {
+    console.log(
+      "%cfunc => useEffect (componentDidMount & componentDidUpdate) A" +
+        ++funcId,
+      funcStyle
+    );
+    document.title = number + " : " + _date;
+    return function () {
+      console.log(
+        "%cfunc => useEffect return (componentDidMount & componentDidUpdate) A" +
+          ++funcId,
+        funcStyle
+      );
+    };
   });
 
-  useEffect(function(){
-    console.log("%cfunc => useEffect (componentDidMount & componentDidUpdate) B" + (++funcId), funcStyle);
-    document.title = number + ' : ' + _date;
+  useEffect(function () {
+    console.log(
+      "%cfunc => useEffect (componentDidMount & componentDidUpdate) B" +
+        ++funcId,
+      funcStyle
+    );
+    document.title = number + " : " + _date;
   });
 
-  console.log("%cfunc => render" + (++funcId), funcStyle);
-  return(
-    <div className='container'>
+  console.log("%cfunc => render" + ++funcId, funcStyle);
+  return (
+    <div className="container">
       <h2>function style compoment</h2>
       <p>Number : {number}</p>
       <p>Date : {_date}</p>
-      <input type="button" value="random" onClick={
-          function(){
-            setNumber(Math.random());
-          }
-        }></input>
-        <input type="button" value="date" onClick={
-          function(){
-            setDate((new Date()).toString());
-          }
-        }></input>
+      <input
+        type="button"
+        value="random"
+        onClick={function () {
+          setNumber(Math.random());
+        }}
+      ></input>
+      <input
+        type="button"
+        value="date"
+        onClick={function () {
+          setDate(new Date().toString());
+        }}
+      ></input>
     </div>
   );
 }
-var classStyle = "color:red"
-class ClassComp extends React.Component{
+var classStyle = "color:red";
+class ClassComp extends React.Component {
   state = {
-    number : this.props.initNumber,
-    date : (new Date()).toString()
-  }
+    number: this.props.initNumber,
+    date: new Date().toString(),
+  };
 
-  componentDidMount(){
+  componentDidMount() {
     console.log("%cclass => componentDidMount", classStyle);
   }
-  shouldComponentUpdate(nextProps, nextState){
+  shouldComponentUpdate(nextProps, nextState) {
     console.log("%cclass => shouldComponentUpdate", classStyle);
     return true;
   }
-  componentDidUpdate(nextProps, nextState){
+  componentDidUpdate(nextProps, nextState) {
     console.log("%cclass => componentDidUpdate", classStyle);
   }
 
-
-  render(){
+  render() {
     console.log("%cclass => component render", classStyle);
-    return(
-      <div className='container'>
+    return (
+      <div className="container">
         <h2>class style compoment</h2>
         <p>Date : {this.state.date}</p>
         <p>Number : {this.state.number}</p>
-        <input type="button" value="random" onClick={
-          function(){
-            this.setState({number:Math.random()})
-          }.bind(this)
-        }></input>
-        <input type="button" value="date" onClick={
-          function(){
-            this.setState({date : (new Date()).toString()})
-          }.bind(this)
-        }></input>
+        <input
+          type="button"
+          value="random"
+          onClick={function () {
+            this.setState({ number: Math.random() });
+          }.bind(this)}
+        ></input>
+        <input
+          type="button"
+          value="date"
+          onClick={function () {
+            this.setState({ date: new Date().toString() });
+          }.bind(this)}
+        ></input>
       </div>
     );
   }
--- components/src/Input.js
+++ components/src/Input.js
@@ -1,19 +1,19 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 const someInput = () => {
-    const [value, setValue] = useState("");
+  const [value, setValue] = useState("");
 
-    const handleChangeValue = (event) => {
-        setValue(event.target.value);
-    }
+  const handleChangeValue = (event) => {
+    setValue(event.target.value);
+  };
 
-    return(
-        <div className='input-container'>
-            <label htmlFor="1">Name</label>
-            <input id="1" type="text" value={value} onChange={handleChangeValue}/>
-        </div>
-    );
-}
+  return (
+    <div className="input-container">
+      <label htmlFor="1">Name</label>
+      <input id="1" type="text" value={value} onChange={handleChangeValue} />
+    </div>
+  );
+};
 
 export default someInput;
